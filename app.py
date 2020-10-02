e425935deba8ded9ff2ee07e5bdaab9f34126fff
Sainath <sainathreddynarahari6@gmail.com>
2020-10-02 21:33:44 +0000

    Update

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World! "
+    return jsonify({'msg':'Hello World!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
