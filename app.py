634bb2a14882a5fa849f08f8279ae7887e4387f8
Sainath <sainathreddynarahari6@gmail.com>
2020-10-02 23:14:15 +0000

    Added Swagger

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, jsonify
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
+
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Learn Flask API's"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
 @app.route("/")
 def index():
