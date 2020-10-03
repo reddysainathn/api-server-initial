a075ab4df78772952f54e415575ecd00733b28d0
Sainath <sainathreddynarahari6@gmail.com>
2020-10-03 04:03:47 +0000

    Updated Swagger

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, url_for
+from flask import Flask, jsonify, render_template, url_for, request
 from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
@@ -14,9 +14,24 @@ SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
 )
 app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
+
 @app.route("/")
 def index():
     return render_template('welcome.html')
+
+
+@app.route("/getInfo")
+def getInfo():
+    return jsonify({'msg': 'Hello World!'})
+
+
+@app.route("/postInfo", methods=['POST'])
+def postInfo():
+    if request.method == 'POST':
+        posted_data = request.get_json()
+        data = posted_data['data']
+        return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
